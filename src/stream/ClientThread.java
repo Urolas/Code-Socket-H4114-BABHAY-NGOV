18aa6b628ef289ce52d30982126e5901ef4d149e
Urolas <44239866+Urolas@users.noreply.github.com>
2021-11-16 11:53:34 +0100

    Revert "FIrst version"
    This reverts commit 55a215e967cfe83e0dfdc79a409a05ab7be300f3.

--- src/stream/ClientThread.java
+++ src/stream/ClientThread.java
@@ -9,64 +9,34 @@ package stream;
 
 import java.io.*;
 import java.net.*;
-import java.util.ArrayList;
-import java.util.List;
 
 public class ClientThread
 	extends Thread {
-
-
-	private Socket clientSocket;
-	private List<ClientThread> clientThreadList = new ArrayList<>();
-	private PrintStream socOut;
 	
-	ClientThread(Socket s,List<ClientThread> listClient) {
-
+	private Socket clientSocket;
+	
+	ClientThread(Socket s) {
 		this.clientSocket = s;
-		this.clientThreadList = listClient;
-
 	}
-
 
  	/**
   	* receives a request from client then sends an echo to the client
+  	* @param clientSocket the client socket
   	**/
 	public void run() {
     	  try {
     		BufferedReader socIn = null;
     		socIn = new BufferedReader(
     			new InputStreamReader(clientSocket.getInputStream()));    
-    		this.socOut = new PrintStream(clientSocket.getOutputStream());
+    		PrintStream socOut = new PrintStream(clientSocket.getOutputStream());
     		while (true) {
     		  String line = socIn.readLine();
-			  String lineWithIp = clientSocket.getInetAddress()+": "+line;
-			  for( ClientThread otherClient: clientThreadList){
-				  if(otherClient.getClientSocket()!=this.getClientSocket()) {
-					  otherClient.getSocOut().println(lineWithIp);
-				  }
-			  }
-
     		  socOut.println(line);
-			  System.out.println(lineWithIp);
     		}
     	} catch (Exception e) {
         	System.err.println("Error in EchoServer:" + e); 
         }
        }
-
-	public void addClientToList(ClientThread newClient) {
-		this.clientThreadList.add(newClient);
-	}
-
-
-	public Socket getClientSocket() {
-		return clientSocket;
-	}
-
-	public PrintStream getSocOut() {
-		return socOut;
-	}
-
   
   }
 
--- src/stream/EchoServerMultiThreaded.java
+++ src/stream/EchoServerMultiThreaded.java
@@ -9,18 +9,16 @@ package stream;
 
 import java.io.*;
 import java.net.*;
-import java.util.ArrayList;
-import java.util.List;
 
 public class EchoServerMultiThreaded  {
   
  	/**
   	* main method
+	* @param EchoServer port
   	* 
   	**/
        public static void main(String args[]){ 
         ServerSocket listenSocket;
-		List<ClientThread> clientThreadList = new ArrayList<>();
         
   	if (args.length != 1) {
           System.out.println("Usage: java EchoServer <EchoServer port>");
@@ -32,12 +30,8 @@ public class EchoServerMultiThreaded  {
 		while (true) {
 			Socket clientSocket = listenSocket.accept();
 			System.out.println("Connexion from:" + clientSocket.getInetAddress());
-			ClientThread ct = new ClientThread(clientSocket,clientThreadList);
+			ClientThread ct = new ClientThread(clientSocket);
 			ct.start();
-			/*for (ClientThread otherClient : clientThreadList){
-				otherClient.addClientToList(ct);
-			}*/
-			clientThreadList.add(ct);
 		}
         } catch (Exception e) {
             System.err.println("Error in EchoServer:" + e);
