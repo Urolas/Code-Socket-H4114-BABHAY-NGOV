a38b88d8ef616894e3d862306fef6da14f21dc03
JuviaBerry <47364179+JuviaBerry@users.noreply.github.com>
2021-11-16 23:58:19 +0100

    Clean

--- src/stream/ClientThread.java
+++ src/stream/ClientThread.java
@@ -21,17 +21,16 @@ public class ClientThread
 
  	/**
   	* receives a request from client then sends an echo to the client
-  	* @param clientSocket the client socket
   	**/
 	public void run() {
     	  try {
-    		BufferedReader socIn = null;
-    		socIn = new BufferedReader(
-    			new InputStreamReader(clientSocket.getInputStream()));    
-    		PrintStream socOut = new PrintStream(clientSocket.getOutputStream());
+			  BufferedReader socIn = new BufferedReader(
+    			new InputStreamReader(clientSocket.getInputStream()));
+			  PrintStream socOut = new PrintStream(clientSocket.getOutputStream());
     		while (true) {
     		  String line = socIn.readLine();
     		  socOut.println(line);
+			  System.out.println(line);
     		}
     	} catch (Exception e) {
         	System.err.println("Error in EchoServer:" + e); 
