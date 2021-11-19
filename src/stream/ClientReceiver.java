16db2b9a6eeba9d6d91afeff54a58f5efa74a3ec
Urolas <44239866+Urolas@users.noreply.github.com>
2021-11-19 08:12:55 +0100

    s

---
+++ src/stream/ClientReceiver.java
@@ -0,0 +1,15 @@
+package stream;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintStream;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
+public class ClientReceiver extends Thread{
+    private Socket clientSocket; //socket
+    public ClientReceiver(Socket s){
+
+    }
+}
